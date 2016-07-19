9dcbc9fd814d06063f9c41ef147511651e64edf8
anuragmath <anurag@theupscale.in>
2016-07-20 00:40:29 +0530

    database migration

--- src/main/java/com/upscale/front/domain/User.java
+++ src/main/java/com/upscale/front/domain/User.java
@@ -27,229 +27,233 @@ import java.time.ZonedDateTime;
 @Document(indexName = "user")
 public class User extends AbstractAuditingEntity implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 
+	@Pattern(regexp = Constants.LOGIN_REGEX)
+	@Size(min = 1, max = 50)
+	@Column(length = 50, unique = true, nullable = false)
+	private String login;
 
-    @Pattern(regexp = Constants.LOGIN_REGEX)
-    @Size(min = 1, max = 50)
-    @Column(length = 50, unique = true, nullable = false)
-    private String login;
+	@JsonIgnore
+	@Size(min = 60, max = 60)
+	@Column(name = "password_hash", length = 60)
+	private String password;
 
-    @JsonIgnore
-    @Size(min = 60, max = 60)
-    @Column(name = "password_hash",length = 60)
-    private String password;
+	@Size(max = 50)
+	@Column(name = "first_name", length = 50)
+	private String firstName;
 
-    @Size(max = 50)
-    @Column(name = "first_name", length = 50)
-    private String firstName;
+	@Size(max = 50)
+	@Column(name = "last_name", length = 50)
+	private String lastName;
 
-    @Size(max = 50)
-    @Column(name = "last_name", length = 50)
-    private String lastName;
+	@Email
+	@Size(max = 100)
+	@Column(length = 100, unique = true)
+	private String email;
 
-    @Email
-    @Size(max = 100)
-    @Column(length = 100, unique = true)
-    private String email;
+	@Size(max = 10)
+	@Column(name = "mobile", length = 10)
+	private String mobile;
 
-    @Size(max = 10)
-    @Column(name = "mobile", length = 10)
-    private String mobile;
+	@NotNull
+	@Column(nullable = false)
+	private boolean activated = false;
 
-    @NotNull
-    @Column(nullable = false)
-    private boolean activated = false;
+	@Size(min = 2, max = 5)
+	@Column(name = "lang_key", length = 5)
+	private String langKey;
 
-    @Size(min = 2, max = 5)
-    @Column(name = "lang_key", length = 5)
-    private String langKey;
+	@Size(max = 20)
+	@Column(name = "activation_key", length = 20)
+	@JsonIgnore
+	private String activationKey;
 
-    @Size(max = 20)
-    @Column(name = "activation_key", length = 20)
-    @JsonIgnore
-    private String activationKey;
+	@Size(max = 20)
+	@Column(name = "reset_key", length = 20)
+	private String resetKey;
 
-    @Size(max = 20)
-    @Column(name = "reset_key", length = 20)
-    private String resetKey;
+	@Column(name = "reset_date", nullable = true)
+	private ZonedDateTime resetDate = null;
 
-    @Column(name = "reset_date", nullable = true)
-    private ZonedDateTime resetDate = null;
+	@Column(name = "request_id", nullable = true)
+	private String requestId;
 
-    @Column(name = "request_id", nullable = true)
-    private String requestId;
+	@JsonIgnore
+	@ManyToMany
+	@JoinTable(name = "jhi_user_authority", joinColumns = {
+			@JoinColumn(name = "user_id", referencedColumnName = "id") }, inverseJoinColumns = {
+					@JoinColumn(name = "authority_name", referencedColumnName = "name") })
+	@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+	private Set<Authority> authorities = new HashSet<>();
 
-    @JsonIgnore
-    @ManyToMany
-    @JoinTable(
-        name = "jhi_user_authority",
-        joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
-        inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "name")})
-    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
-    private Set<Authority> authorities = new HashSet<>();
+	@Column(name = "client_id", nullable = true)
+	private Long clientId;
 
-    @Column(name = "client_id", nullable = true)
-    private Long clientId;
+	@Column(name = "self_service_id", nullable = false)
+	private Long selfServiceId;
 
-    @Column(name = "self_service_id", nullable = false)
-    private Long selfServiceId;
+	public Long getId() {
+		return id;
+	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
 
+	public Long getClientId() {
+		return clientId;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setClientId(Long id) {
+		this.clientId = id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public Long getSelfServiceId() {
+		return selfServiceId;
+	}
 
-    public Long getClientId() {return clientId; }
+	public void setSelfServiceId(Long id) {
+		this.selfServiceId = id;
+	}
 
-    public void setClientId(Long id) {this.clientId = id;}
+	public String getLogin() {
+		return login;
+	}
 
-    public Long getSelfServiceId() {return selfServiceId; }
+	public String getRequestId() {
+		return requestId;
+	}
 
-    public void setSelfServiceId(Long id) { this.selfServiceId = id;}
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
 
-    public String getLogin() {
-        return login;
-    }
+	// Lowercase the login before saving it in database
+	public void setLogin(String login) {
+		this.login = login.toLowerCase(Locale.ENGLISH);
+	}
 
-    public String getRequestId() {return requestId;}
+	public String getPassword() {
+		return password;
+	}
 
-    public void setRequestId(String requestId){this.requestId = requestId;}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    //Lowercase the login before saving it in database
-    public void setLogin(String login) {
-        this.login = login.toLowerCase(Locale.ENGLISH);
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getMobile() {
+		return mobile;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public String getMobile() {return mobile; }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public void setMobile(String mobile){this.mobile = mobile;}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public boolean getActivated() {
+		return activated;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setActivated(boolean activated) {
+		this.activated = activated;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getActivationKey() {
+		return activationKey;
+	}
 
-    public boolean getActivated() {
-        return activated;
-    }
+	public void setActivationKey(String activationKey) {
+		this.activationKey = activationKey;
+	}
 
-    public void setActivated(boolean activated) {
-        this.activated = activated;
-    }
+	public String getResetKey() {
+		return resetKey;
+	}
 
-    public String getActivationKey() {
-        return activationKey;
-    }
+	public void setResetKey(String resetKey) {
+		this.resetKey = resetKey;
+	}
 
-    public void setActivationKey(String activationKey) {
-        this.activationKey = activationKey;
-    }
+	public ZonedDateTime getResetDate() {
+		return resetDate;
+	}
 
-    public String getResetKey() {
-        return resetKey;
-    }
+	public void setResetDate(ZonedDateTime resetDate) {
+		this.resetDate = resetDate;
+	}
 
-    public void setResetKey(String resetKey) {
-        this.resetKey = resetKey;
-    }
+	public String getLangKey() {
+		return langKey;
+	}
 
-    public ZonedDateTime getResetDate() {
-       return resetDate;
-    }
+	public void setLangKey(String langKey) {
+		this.langKey = langKey;
+	}
 
-    public void setResetDate(ZonedDateTime resetDate) {
-       this.resetDate = resetDate;
-    }
+	public Set<Authority> getAuthorities() {
+		return authorities;
+	}
 
-    public String getLangKey() {
-        return langKey;
-    }
+	public void setAuthorities(Set<Authority> authorities) {
+		this.authorities = authorities;
+	}
 
-    public void setLangKey(String langKey) {
-        this.langKey = langKey;
-    }
-    
-    public Set<Authority> getAuthorities() {
-        return authorities;
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
 
-    public void setAuthorities(Set<Authority> authorities) {
-        this.authorities = authorities;
-    }
+		User user = (User) o;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+		if (!login.equals(user.login)) {
+			return false;
+		}
 
-        User user = (User) o;
+		return true;
+	}
 
-        if (!login.equals(user.login)) {
-            return false;
-        }
+	@Override
+	public int hashCode() {
+		return login.hashCode();
+	}
 
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        return login.hashCode();
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-            "login='" + login + '\'' +
-            ", firstName='" + firstName + '\'' +
-            ", lastName='" + lastName + '\'' +
-            ", email='" + email + '\'' +
-            ", mobile='" + mobile + '\'' +
-            ", activated='" + activated + '\'' +
-            ", langKey='" + langKey + '\'' +
-            ", clientId'" + clientId + '\'' +
-            ", selfServiceId'" + selfServiceId + '\'' +
-            ", activationKey='" + activationKey + '\'' +
-            "}";
-    }
+	@Override
+	public String toString() {
+		return "User{" + "login='" + login + '\'' + ", firstName='" + firstName + '\'' + ", lastName='" + lastName
+				+ '\'' + ", email='" + email + '\'' + ", mobile='" + mobile + '\'' + ", activated='" + activated + '\''
+				+ ", langKey='" + langKey + '\'' + ", clientId'" + clientId + '\'' + ", selfServiceId'" + selfServiceId
+				+ '\'' + ", activationKey='" + activationKey + '\'' + "}";
+	}
 }
